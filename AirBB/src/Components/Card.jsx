79d3b8722c00967a14a0420d57bc405837c1d299
allentevyaw <atevyaw@gmail.com>
2022-12-21 18:35:42 -0500

    December21

--- AirBB/src/Components/Card.jsx
+++ AirBB/src/Components/Card.jsx
@@ -8,12 +8,12 @@ export const Card = (props) => {
             <img src={props.img} className="card--image" />
             <div className="card--stats">
                 <img src={ star } className="card--star" />
-                <span>{props.rating}</span>
+                <span>({props.rating})</span>
                 <span className="grey">{props.reviewCount}</span>
                 <span className="grey">{props.country}</span>
             </div>
             <p>{props.title}</p>
-            <p><span className="bold">{props.price}</span> / person</p>
-        </div>
+            <p><span className="bold">${props.price}</span> / person</p>
+        </div> 
     )
 }
